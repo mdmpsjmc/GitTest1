79d64c783b79253dfa7eef14e1579201a49f0c12
John Cooley <john.cooley@manchester.ac.uk>
2020-10-22 12:17:16 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Code was added in GitHub
             Console.WriteLine("Hello World!");
         }
     }
